9ed057bdc14a16d6c92a551da29a3dd14b7548b9
Cleverson de Moraes <cleverson.moraes93@gmail.com>
2022-01-27 18:45:42 -0300

    Segundo commit

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const botao = document.getElementById("botao")
 const body = document.getElementsByTagName("body")
+const text_button = document.getElementById("text_button")
 
 function ligaDesliga() {
     botao.classList.toggle("ligaDesliga")
     if (botao.classList.contains("ligaDesliga")) {
         document.body.style.backgroundColor = "white"
+        text_button.innerHTML = "off"
     } else {
         document.body.style.backgroundColor = "black"
+        text_button.innerHTML = "on"
     }
 }
